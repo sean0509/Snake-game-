ef4b02838e9ec089fb45b22fdfff3db5780bfd7e
sean0509 <shawnfer0509@gmail.com>
2025-08-16 12:19:51 +0530

    initial commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 600;
+        int boardheight = boardwidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Snakegame snakegame = new Snakegame(boardwidth, boardheight);
+        frame.add(snakegame);
+        frame.pack();
+        snakegame.requestFocus();
+    }
+
+}
